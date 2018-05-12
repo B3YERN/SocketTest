d9c2d0e58b5cb478b16b41f65f1f7a1ca6c44146
B3YERN <36043144+B3YERN@users.noreply.github.com>
2018-05-12 09:07:14 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,10 +2,27 @@
 import socket
 import os
 import builtins
+import urllib.request
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = socket.gethostbyname(socket.gethostname())
+    port = 5050
+    ip = urllib.request.urlopen('http://ip.42.pl/raw').read()
+    print(str(ip.decode('utf-8')))
     print(str(host))
 
-main()
+    s.bind((host,port))
+
+    s.listen()
+    print('listening...')
+    c,addr = s.accept()
+    print('connected to : '+str(addr))
+
+    while True:
+        msg = c.recv(1024).decode('utf-8')
+        print(str(msg))
+
+if __name__=='__main__':
+    main()
+    
